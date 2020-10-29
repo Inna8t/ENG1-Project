eb80897c2eb0738c99bc0f4b54f694349d55ad05
VireV <57366614+VireV@users.noreply.github.com>
2020-10-29 14:50:54 +0000

    Extra generated stuff

--- AuberProject/desktop/src/dev/enigma/auber/desktop/DesktopLauncher.java
+++ AuberProject/desktop/src/dev/enigma/auber/desktop/DesktopLauncher.java
@@ -6,8 +6,12 @@ import dev.enigma.auber.MainGame;
 
 public class DesktopLauncher
 {
-	public static void main (String[] arg) {
+	public static void main (String[] arg)
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MainGame(), config);
+
+
+
 	}
 }
