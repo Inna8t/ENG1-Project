6cb87176925aaaf3df14ccc541e7a9328f88e540
VireV <57366614+VireV@users.noreply.github.com>
2020-10-29 14:31:31 +0000

    Project files
    I generated project files for the game.

---
+++ AuberProject/desktop/src/dev/enigma/auber/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package dev.enigma.auber.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.enigma.auber.MainGame;
+
+public class DesktopLauncher
+{
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
